1b0795ada6e54cda2a2e4a7b70e2eb9e475f8e8a
J-Okoto <okotojr@gmail.com>
2020-11-01 19:37:24 +0300

    created superuser and added models to admin page +added migrations to the database

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import neighbourhood,notifications,Profile
+
+
+
+
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Profile)
+admin.site.register(notifications)
--- hoodapp/models.py
+++ hoodapp/models.py
@@ -6,6 +6,22 @@ import datetime as dt
 
 
 # Create your models here.
+
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
+
+
+
 class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/')
     description = HTMLField()
@@ -16,3 +32,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class notifications(models.Model):
+    title = models.CharField(max_length=100)
+    notification = HTMLField()
+    
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
