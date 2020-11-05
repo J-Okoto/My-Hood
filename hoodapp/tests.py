9079e1adc8c711d7ed16f8b57a495a2bba8d4651
J-Okoto <okotojr@gmail.com>
2020-11-05 11:05:59 +0300

    added profile test class and updated neighbourhood test class

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -1,27 +1,54 @@
 
 
 from django.test import TestCase
-from .models import neighbourhood
+from .models import neighbourhood,Profile
 from django.contrib.auth.models import User
-import datetime as dt
-# Create your tests here.
-class neighbourhoodTestClass(TestCase):
-    def setUp(self):
-        self.Emba = neighbourhood(neighbourhood='Emba')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.Emba,neighbourhood))
+# Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.hood = Neighbourhood(
+            name='Emba')
+        self.hood.save_Neighbourhood()
 
     def tearDown(self):
-        neighbourhood.objects.all().delete()
+        Neighbourhood.objects.all().delete()
 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood, Neighbourhood))
+
+        # Testing Save Method
     def test_save_method(self):
-        self.Emba.save_neighbourhood()
-        hood = neighbourhood.objects.all()
-        self.assertTrue(len(hood)>0)
+        hood = Neighbourhood.objects.all()
+        self.assertTrue(len(hood) > 0)
 
     def test_delete_method(self):
-        self.Emba.delete_neighbourhood('Emba')
-        hood = neighbourhood.objects.all()
-        self.assertTrue(len(hood)==0)
+        self.hood.delete_Neighbourhood()
+        hood = Neighbourhood.objects.all()
+        self.assertTrue(len(hood) == 0)
 
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.neighbourhood = Neighbourhood(name="Emba")
+        self.neighbourhood.save()
+        self.user = Profile(name='Joel', email='okotojr@gmail.com', neighbourhood=self.neighbourhood,
+                                 username="Joe", description="Cool&Calm")
+        self.user.save_User()
+
+    def tearDown(self):
+        Neighbourhood.objects.all().delete()
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user,Profile))
+
+        # Testing Save Method
+    def test_save_method(self):
+        user =Profile.objects.all()
+        self.assertTrue(len(user) > 0)
+
+    def test_delete_method(self):
+        self.user.delete_User()
+        user =Profile.objects.all()
+        self.assertTrue(len(user) == 0)
