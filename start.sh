205a72c58596d42bda7754f1f96c60c8042a3b1f
J-Okoto <okotojr@gmail.com>
2020-11-05 16:55:05 +0300

    deploying to heroku

---
+++ start.sh
@@ -0,0 +1,11 @@
+SECRET_KEY='9ki#ym91%4_nx4wjy1g@bgtzsx8k6w^bd6lfjh!g$u)#g1xa&3'
+DEBUG=false #set to false in production
+DB_NAME='del5rbajv5s46e'
+DB_USER='bkiulxevvcxfbk'
+DB_PASSWORD='603401a1d233f99dbaa0d686343afb92a18d3269cc846ad54c7840d4b3405eaf'
+DB_HOST='ec2-34-204-121-199.compute-1.amazonaws.com'
+DB_PORT='5432'
+ALLOWED_HOSTS='*'
+DISABLE_COLLECTSTATIC=1
+
+postgres://bkiulxevvcxfbk:603401a1d233f99dbaa0d686343afb92a18d3269cc846ad54c7840d4b3405eaf@ec2-34-204-121-199.compute-1.amazonaws.com:5432/del5rbajv5s46e
